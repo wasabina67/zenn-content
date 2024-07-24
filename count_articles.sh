cffa55bf7024b6d332cbcbcc440c0412f928b4c4
wasabina67 <wasabina67@gmail.com>
2024-07-25 00:37:18 +0900

    Update count_articles.sh

--- count_articles.sh
+++ count_articles.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+PUB=`grep "published: true" articles/*.md | wc -l`
+UNPUB=`grep "published: false" articles/*.md | wc -l`
+TOTAL=$((PUB + UNPUB))
+
+echo "- Published: ${PUB}"
+echo "- Unpublished: ${UNPUB}"
+echo "- Total: ${TOTAL}"
