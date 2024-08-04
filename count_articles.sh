4eeb88049616ddd361eba7b22a5b3f1a694b9800
wasabina67 <wasabina67@gmail.com>
2024-08-04 11:48:07 +0900

    Update count_articles.sh

--- count_articles.sh
+++ count_articles.sh
@@ -4,6 +4,10 @@ PUB=`grep "published: true" articles/*.md | wc -l`
 UNPUB=`grep "published: false" articles/*.md | wc -l`
 TOTAL=$((PUB + UNPUB))
 
+sed -i "s/^- Published:.*/- Published: ${PUB}/" README.md
+sed -i "s/^- Unpublished:.*/- Unpublished: ${UNPUB}/" README.md
+sed -i "s/^- Total:.*/- Total: ${TOTAL}/" README.md
+
 echo "- Published: ${PUB}"
 echo "- Unpublished: ${UNPUB}"
 echo "- Total: ${TOTAL}"
