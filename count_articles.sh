818ab3ad94c5c2e299b7e85687f06045cd42b9ac
wasabina67 <wasabina67@gmail.com>
2024-08-10 03:11:17 +0900

    Update count_articles.sh

--- count_articles.sh
+++ count_articles.sh
@@ -4,8 +4,8 @@ PUB=`grep "published: true" articles/*.md | wc -l`
 UNPUB=`grep "published: false" articles/*.md | wc -l`
 TOTAL=$((PUB + UNPUB))
 
-sed -i "s/^- Published:.*/- Published: ${PUB}/" README.md
-sed -i "s/^- Unpublished:.*/- Unpublished: ${UNPUB}/" README.md
+sed -i "s/^  - Published:.*/  - Published: ${PUB}/" README.md
+sed -i "s/^  - Unpublished:.*/  - Unpublished: ${UNPUB}/" README.md
 sed -i "s/^- Total:.*/- Total: ${TOTAL}/" README.md
 
 if ! git diff --quiet README.md; then
