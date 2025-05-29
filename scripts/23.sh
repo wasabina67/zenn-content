711debb2114b1c5f42d4aac2789338bfed1a8019
wasabina67 <wasabina67@gmail.com>
2025-05-29 20:56:50 +0900

    update

--- scripts/23.sh
+++ scripts/23.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-echo 'The number of args is 3.'
+echo 'The number of arguments is 3.'
 
 echo $1
 echo $2
@@ -14,7 +14,7 @@ echo $3
 echo 'Hello, World!'
 
 if [ "$?" -eq 0 ]; then
-    echo 'Command succeed'
+    echo 'Command succeeded.'
 else
     echo 'Command failed.'
 fi
