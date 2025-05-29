4a4bacf2d508bdb3622244f961adf5219bcefb00
wasabina67 <wasabina67@gmail.com>
2025-05-29 20:33:10 +0900

    Update articles/22-06b8ngsl89tikbg5.md & Create scripts/22.sh

--- scripts/22.sh
+++ scripts/22.sh
@@ -1,13 +1,3 @@
----
-title: "Bashで実行時間を計測する"
-emoji: "⏰"
-type: "tech"
-topics: ["bash"]
-published: true
-published_at: 2025-05-29
----
-
-```bash
 #!/bin/bash
 
 BEGIN=$(date +'%s.%3N')
@@ -18,4 +8,3 @@ DURING=$(echo "scale=3; ${END} - ${BEGIN}" | bc)
 echo "BEGIN: ${BEGIN}"
 echo "END: ${END}"
 echo "DURING: ${DURING}"
-```
