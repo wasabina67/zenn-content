433dc5e1c454828bce7956bbbdf320c829df0514
wasabina67 <wasabina67@gmail.com>
2025-06-03 18:29:36 +0900

    update

--- scripts/29.py
+++ scripts/29.py
@@ -1,1 +1,29 @@
+import hashlib
+import os
+import random
+import string
 
+
+def gen(length, chars):
+    return ''.join(random.choice(chars) for _ in range(length))
+
+
+def func_1():
+    return gen(
+        20,
+        string.digits + string.ascii_lowercase + string.ascii_uppercase
+    )
+
+
+def func_2():
+    return hashlib.sha256(os.urandom(32)).hexdigest()[:20]
+
+
+def func_3():
+    return gen(20, string.ascii_letters + string.digits + '_')
+
+
+if __name__ == '__main__':
+    print(func_1())
+    print(func_2())
+    print(func_3())
