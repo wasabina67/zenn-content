70485b6027d1aa35f47ea596c303fde370088ded
wasabina67 <wasabina67@gmail.com>
2025-06-03 19:51:53 +0900

    update

--- scripts/31.py
+++ scripts/31.py
@@ -2,8 +2,15 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def main():
-    pass
+def main(from_email, to_email, smtp_server, port, password):
+    message = "message"
+    mime_text = MIMEText(message, "html")
+    mime_text["Subject"] = "Subject"
+    mime_text["From"] = from_email
+    mime_text["To"] = to_email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(from_email, password)
+        server.send_message(mime_text)
 
 
 if __name__ == "__main__":
