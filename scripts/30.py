e05841c764a8215c944a59e4f6e5418a69b72213
wasabina67 <wasabina67@gmail.com>
2025-06-03 19:10:41 +0900

    update

--- scripts/30.py
+++ scripts/30.py
@@ -4,6 +4,18 @@ def r(nums):
     result = []
     start = nums[0]
 
+    for i, num in enumerate(nums[1:], 1):
+        if num - nums[i-1] != 1:
+            result.append(
+                str(start) if start == nums[i-1] else f"{start}~{nums[i-1]}"
+            )
+            start = num
+
+    result.append(
+        str(start) if start == nums[-1] else f"{start}~{nums[-1]}"
+    )
+    return result
+
 
 numbers = [1, 2, 3, 4, 5, 8, 9, 10, 12, 13]
 print(r(numbers))
